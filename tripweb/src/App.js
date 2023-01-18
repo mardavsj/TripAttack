b4a771f8888dd8bb1e3ea0bdbb05f476c9d1bb3f
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-01-18 15:36:23 +0530

    react setup

---
+++ tripweb/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-5xl font-bold text-center justify-center">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
