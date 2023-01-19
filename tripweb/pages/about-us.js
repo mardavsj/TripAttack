86b0bc31daecb8e47b4d92fb04beebbae4e88c99
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-01-19 21:00:56 +0530

    navbar

---
+++ tripweb/pages/about-us.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <title>About Us</title>
+      </Head>
+    <div>Aboutus</div>
+    </>
+  )
+}
+
+export default Page
---
+++ tripweb/pages/destinations.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const destinations = () => {
+  return (
+    <>
+    <Head>
+        <title>Destinations</title>
+    </Head>
+    <div>destinations</div>
+    </>
+  )
+}
+
+export default destinations
