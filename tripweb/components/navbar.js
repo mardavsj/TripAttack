20001ab78201983d3ec0f40aaf1d5b1d32aa7473
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-03-03 21:16:57 +0530

    navbar

--- tripweb/components/navbar.js
+++ tripweb/components/navbar.js
@@ -7,8 +7,8 @@ const navbar = () => {
               <div className="shrink w-80 ">
                 <a className="font-bold text-xl text-gray-800">TripAttack</a>
             </div>
-              <div className="md:flex-none md:w-96 w-72  flex justify-center py-4 sm:py-0">
-                <input type="text" className="mt-1 block w-96 px-3 py-2 bg-white border-slate-300 rounded-md text-sm shadow-md placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Select your preferred states" />
+              <div className="md:flex-none md:w-96 w-72 flex justify-center py-4 sm:py-0">
+                <input type="text" className="mt-1 block w-96 px-3 bg-white border-slate-300 rounded-md text-sm placeholder-slate-500 shadow-md shadow-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Select your preferred states" />
             </div>
             <div className="w-96 flex justify-center">
                 <div className="flex gap-6 text-gray-800">
